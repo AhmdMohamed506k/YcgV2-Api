82d1b9d9783decfa7ff6520b028f0d60e8d190ac
${AhmedMohamed} <154832668+AhmdMohamed506k@users.noreply.github.com>
2026-04-13 21:53:30 +0200

    Update Payment.routes.js

--- src/modules/users/Payment/Payment.routes.js
+++ src/modules/users/Payment/Payment.routes.js
@@ -1,8 +1,7 @@
 import { Router } from "express";
-import express from "express"; 
 import { auth } from "../../../middleware/Auth/auth.js";
 import * as PY from "./Payment.controller.js"
-
+import express from "express"; 
 
 
 
@@ -10,7 +9,7 @@ const PaymentRouter= Router()
 
 
 
-PaymentRouter.post('/subscribe', auth, PY.createSubscription);
+PaymentRouter.post('/subscribe', auth, PY.CreateCheckoutSession);
 
 PaymentRouter.post('/webhook',  express.raw({ type: 'application/json' }), PY.stripeWebhook);
 
