95e58298ea4591f8b44106853707d252ecd4f058
${AhmedMohamed} <154832668+AhmdMohamed506k@users.noreply.github.com>
2026-04-21 01:16:20 +0200

    new update101

--- src/modules/Companys/Company/Company.controller.js
+++ src/modules/Companys/Company/Company.controller.js
@@ -5,6 +5,12 @@ import { asyncHandler } from "../../../middleware/asyncHandler/asyncHandler.js";
 import cloudinary from "../../../utils/Cloudinary/Cloudinary.js";
 import redisClient from "../../../utils/redisClient/redisClient.js";
 
+
+
+
+
+
+// Company_Page_CRUD
 export const CreateCompanyPage = asyncHandler(async (req, res, next) => {
   const { CompanyName, ContactEmail, Industry, OrganizationSize, OrganizationType, Website, Location, Description,} = req.body;
   
@@ -44,12 +50,18 @@ export const CreateCompanyPage = asyncHandler(async (req, res, next) => {
 
   return res.status(201).json({status: "success",message: "Company registered successfully",company,});
 });
-
-//Need Editing
-export const GetSpeceficCompanyInfo = asyncHandler(async (req, res, next) => {
+export const GetSpeceficCompanyDashBoard = asyncHandler(async (req, res, next) => {//Need Editing
 
 
   const userId = req.user._id;
+  const resultObject={
+    CompanyInfo:{
+      
+    },
+    companyPostsCount:0,
+    companyPosts:{}
+  }
+
 
 
   const CashKey = `CompanyDashboard:${userId}`;
@@ -57,30 +69,47 @@ export const GetSpeceficCompanyInfo = asyncHandler(async (req, res, next) => {
   if (cachedData) {
     return res.status(200).json({status: "success",source: "cache",data: JSON.parse(cachedData),});
   }
+ 
 
+
+  
+  const companyExists = await companyModel.findOne({"Admins.user":userId});
+  if (!companyExists) return next(new Error("Company not found", 404));  
      
-    const currentAdmin = company.Admins.find(a => a.user.toString() === userId.toString());
-    if (!currentAdmin || !["admin", "superAdmin"].includes(currentAdmin.role)) {
-      return next(new Error("Unauthorized: Only admins can post", 403));
-    }
-
-  const company = await companyModel.findOne({ "Admins.user": userId }).populate("CompanyFollowers").populate("CompanyFollowersCount").populate("CompanyFollowingCount").populate("CompanyViewsCount");
-  if (!company) {
-    return next(new Error("Company not found or you don't have access"), 404);
+  const currentAdmin = companyExists.Admins.find(a => a.user.toString() === userId.toString());
+  if (!currentAdmin || !["admin", "superAdmin"].includes(currentAdmin.role)) {
+    return next(new Error("Unauthorized: Only admins can post", 403));
   }
 
 
 
 
+  const company = await companyModel.findOne({ "Admins.user": userId })
+  .populate("CompanyFollowers")
+  .populate("CompanyFollowersCount")
+  .populate("CompanyFollowingCount")
+  .populate("CompanyViewsCount");
+
+  resultObject.CompanyInfo=company;
 
 
+  const compantPosts= await ActivityModel.find({CreatedBy:company._id})
+
+  
+  resultObject.companyPosts=compantPosts;
+  resultObject.companyPostsCount=compantPosts.length
 
 
-  await redisClient.set(CashKey, JSON.stringify(company), { EX: 300 });
+  if (!company) {
+    return next(new Error("Company not found or you don't have access"), 404);
+  }
 
-  res.status(200).json({ status: "Success", source: "DB", data: company });
+  
+
+  await redisClient.set(CashKey, JSON.stringify(resultObject), { EX: 300 });
+
+  res.status(200).json({ status: "Success", source: "DB", data: resultObject });
 });
-
 export const updateCompany = asyncHandler(async (req, res, next) => {
 
 
@@ -137,7 +166,115 @@ export const updateCompany = asyncHandler(async (req, res, next) => {
     company: updatedCompany,
   });
 });
+export const deleteCompany = asyncHandler(async (req, res, next) => {
+  const { companyId } = req.params;
+  const userId = req.user._id;
 
+
+  const company = await companyModel.findById(companyId);
+  if (!company) return next(new Error("Company not found", 404));
+
+  
+  const isEmployeeValid = company.HRManagers?.some((hr) => hr.user.toString() === userId.toString() && hr.status === "active");
+  if (!isEmployeeValid) {return next(new Error("Security Alert: User not found in HR records or inactive", 401));}
+
+ 
+  const currentAdmin = company.Admins.find((admin) => admin.user.toString() === userId.toString());
+  if (!currentAdmin || currentAdmin.role !== "superAdmin") {
+    return next(new Error("Unauthorized: Only an active Super Admin can delete the page", 403));
+  }
+
+  const mediaToDelete = [company.Logo?.public_id, company.Banner?.public_id].filter(Boolean);
+  if (mediaToDelete.length > 0) { await Promise.all(mediaToDelete.map(id => cloudinary.uploader.destroy(id)));}
+
+  
+  await ActivityModel.deleteMany({ CreatedBy: companyId });
+
+ 
+  await companyModel.findByIdAndDelete(companyId);
+
+
+  const keys = await redisClient.keys(`Feed:${companyId}*`);
+  if (keys.length > 0) await redisClient.del(keys);
+
+  res.status(200).json({status: "success",message: "Company deleted securely after multi-factor authorization."});
+});
+//////////////
+
+
+
+// Company_Page_activity
+export const getAllCompanyActivities = asyncHandler(async (req, res, next) => {
+  const { companyId } = req.params; 
+  const { page = 1, limit = 10 } = req.query; 
+  const skip = (page - 1) * limit;
+
+
+  const cashKey = `Feed:${companyId}:p:${page}:l:${limit}`;
+  const cashedData = await redisClient.get(cashKey);
+
+  
+  if (cashedData) {
+    return res.status(200).json({ status: "success", source: "Cache", data: JSON.parse(cashedData) });
+  }
+
+  const result = {
+    totalCount: 0, 
+    data: [],
+    companyInfo: {}
+  };
+
+
+  const company = await companyModel.findById(companyId).select("CompanyName Logo Location");
+  if (!company) return next(new Error("Company not found", 404));
+  result.companyInfo = company;
+
+  
+  const [activities, total] = await Promise.all([
+    ActivityModel.find({ CreatedBy: companyId })
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(Number(limit))
+      .populate({
+          path: "comments",
+          populate: { path: "addedBy", select: "firstName lastName profileImage" } 
+      }),
+    ActivityModel.countDocuments({ CreatedBy: companyId })
+  ]);
+
+  result.data = activities;
+  result.totalCount = total;
+
+  await redisClient.set(cashKey, JSON.stringify(result), { EX: 300 });
+  res.status(200).json({ status: "success", source: "DB", result });
+});
+export const getSpecificCompanyActivityInfo = asyncHandler(async (req, res, next) => {
+  const { activityID, CompanyId } = req.body;
+
+  const cashKey = `ActivityDetail:${activityID}`; 
+  const cashedData = await redisClient.get(cashKey);
+
+  if (cashedData) {
+    return res.status(200).json({ status: "success", source: "Cache", data: JSON.parse(cashedData) });
+  }
+
+  const result = { data: {}, companyInfo: {} };
+
+  const company = await companyModel.findById(CompanyId).select("CompanyName Logo Location");
+  if (!company) return next(new Error("Company not found", 404));
+  result.companyInfo = company;
+
+  const activity = await ActivityModel.findById(activityID).populate({
+        path: "comments",
+        populate: { path: "addedBy", select: "firstName lastName profileImage" }
+    });
+
+  if (!activity) return next(new Error("Activity not found", 404));
+  result.data = activity;
+
+  await redisClient.set(cashKey, JSON.stringify(result), { EX: 600 });
+  res.status(200).json({ status: "success", source: "DB", result });
+});
 export const createActivity = asyncHandler(async (req, res, next) => {
   const { text, creatorType, companyId } = req.body;
   const userId = req.user._id;
@@ -195,8 +332,82 @@ export const createActivity = asyncHandler(async (req, res, next) => {
 
   res.status(201).json({ status: "success", data: activity });
 });
+export const updateSpecificActivityInfo = asyncHandler(async (req, res, next) => {
+  const { text } = req.body;
+  const { activityId } = req.params; 
+  const userId = req.user._id;
+
+  const activity = await ActivityModel.findById(activityId);
+  if (!activity) return next(new Error("Activity not found", 404));
+
+ 
+  if (activity.creatorType === "Company") {
 
 
+    const company = await companyModel.findOne({ _id: activity.CreatedBy, "Admins.user": userId });
+    const currentAdmin = company?.Admins.find(a => a.user.toString() === userId.toString());
+    
+    if (!currentAdmin || !["admin", "superAdmin"].includes(currentAdmin.role)) {
+      return next(new Error("Unauthorized: You must be an admin of this company", 403));
+    }
+  } else {
+    if (activity.addedBy.toString() !== userId.toString()) {
+      return next(new Error("Unauthorized: This isn't your post", 403));
+    }
+  }
+
+  if (!text || text === activity.text) {
+    return next(new Error("No changes detected or empty text", 400));
+  }
+
+  activity.text = text;
+  await activity.save();
+
+
+  await redisClient.del(`Feed:${activity.CreatedBy}`);
+
+  
+  res.status(200).json({ status: "success", message: "Post Updated Successfully", data: activity });
+});
+export const DeleteActivity = asyncHandler(async (req, res, next) => {
+  const { activityId } = req.params; 
+  const userId = req.user._id;
+
+  const activity = await ActivityModel.findById(activityId);
+  if (!activity) return next(new Error("Activity not found", 404));
+
+
+  if (activity.creatorType === "Company") {
+    const company = await companyModel.findOne({ _id: activity.CreatedBy, "Admins.user": userId });
+    if (!company) return next(new Error("Unauthorized admin access", 403));
+  } else {
+    if (activity.addedBy.toString() !== userId.toString()) {
+      return next(new Error("Unauthorized", 403));
+    }
+  }
+
+ 
+  if (activity.media?.public_id) {
+    const resourceType = activity.ActivityType === "video" ? "video" : "image";
+    await cloudinary.uploader.destroy(activity.media.public_id, { resource_type: resourceType });
+  }
+
+  await ActivityModel.findByIdAndDelete(activityId);
+  await redisClient.del(`Feed:${activity.CreatedBy}`);
+
+  res.status(200).json({ status: "success", message: "Deleted Successfully" });
+});
+//////////////
+
+
+//activities_Services
+
+
+
+
+
+
+// Page_Services 
 export const addAdminToCompany = asyncHandler(async (req, res, next) => {
   const { companyId } = req.params;
   const { newUserEmail, role } = req.body;
--- src/modules/Companys/Company/Company.routes.js
+++ src/modules/Companys/Company/Company.routes.js
@@ -7,15 +7,27 @@ import { MulterHost, validExtensions } from "../../../middleware/MulterHost/Mult
 
 const CompanysRouter = Router()
 
-CompanysRouter.get("/GetCompanyDashboardData",auth,CP.GetSpeceficCompanyInfo)
-CompanysRouter.post("/CreateCompanyPage",auth,MulterHost(validExtensions.image).single("Logo"),CP.CreateCompanyPage)
-CompanysRouter.post("/CreateCompanyActivity", auth,MulterHost([...validExtensions.image, ...validExtensions.media]).fields([
-        { name: 'image', maxCount: 1 }, 
-        { name: 'video', maxCount: 1 }, 
-        { name: 'cover', maxCount: 1 },
-    ]),  CP.createActivity);
-CompanysRouter.put("/UpdateCompanyInfo/:companyId",auth,MulterHost(validExtensions.image).single("Logo"),CP.updateCompany)
-CompanysRouter.post("/AddNewAdmin/:companyId",auth,CP.addAdminToCompany)
+
+// Company_Page_CRUD
+CompanysRouter.get("/GetCompanyDashboardData",auth,CP.GetSpeceficCompanyDashBoard);//done
+CompanysRouter.post("/CreateCompanyPage",auth,MulterHost(validExtensions.image).single("Logo"),CP.CreateCompanyPage);//done
+CompanysRouter.put("/UpdateCompanyInfo/:companyId",auth,MulterHost(validExtensions.image).single("Logo"),CP.updateCompany)//done
+CompanysRouter.delete("/DeleteCompany/:companyId", auth , CP.deleteCompany);
+//////////////
+
+
+// Company_Page_activity
+CompanysRouter.get( "/GetallActivities/:companyId",auth, CP.getAllCompanyActivities); //done
+CompanysRouter.get( "/GetSpecificActivitiesInfo",auth, CP.getSpecificCompanyActivityInfo);
+CompanysRouter.post("/CreateCompanyActivity", auth,MulterHost([...validExtensions.image, ...validExtensions.media]).fields([{ name: 'image', maxCount: 1 },{ name: 'video', maxCount: 1 },{ name: 'cover', maxCount: 1 }, ]),  CP.createActivity);//done
+CompanysRouter.patch( "/UpdateCompanyActivity/:activityId",auth, CP.updateSpecificActivityInfo);//done
+CompanysRouter.delete("/DeleteCompanyActivity/:activityId",auth, CP.DeleteActivity);//done
+///////////
+
+
+
+// Page_Services
+CompanysRouter.post("/AddNewAdmin/:companyId",auth,CP.addAdminToCompany)//done
 
 
 
